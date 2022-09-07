0d40744008315b4f4e12398ee08410e99e24fb91
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-09-07 16:31:05 +0530

    Create add.js

---
+++ add.js
@@ -0,0 +1,7 @@
+//add two numbers entered by the user
+const number1=parseInt(prompt('Enter the first number'));
+const number2=parseInt(prompt('Enter the second number'));
+const sum=number1+number2;
+console.log('The sum of ${number1} and ${number2} is ${sum}');
+
+                
