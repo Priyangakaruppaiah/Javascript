fa139778ebfdf3057e3032b9794afaa8ef6d3ae8
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-01 18:06:56 +0530

    Day-8 completed

---
+++ 8.Operators.js
@@ -0,0 +1,69 @@
+let x=12
+let y=192
+//ARITHMETIC OPERATORS
+console.log(x+y)
+
+console.log(x-y)
+
+console.log(x*y)
+
+console.log(x/y)
+
+console.log(++x)
+
+console.log(y++)
+//ASSIGNMENT OPERATORS
+
+console.log(x+=5)
+
+//COMPARISON OPERATORS
+
+console.log(x==y)
+
+console.log(x===y)
+
+console.log(x!==y)
+
+const isValidNumber=x>45 && y>90
+console.log(isValidNumber)
+
+const var1="test"
+const var2="test"
+console.log(var1==var2)
+console.log(var1===var2)
+
+const var11=10
+const var22="10"
+console.log(var11==var22)
+console.log(var11===var22)
+
+
+const var111=null
+const var222=undefined
+console.log(var111==var222)
+console.log(var111===var222)
+
+//LOGICAL OPERATORS
+
+console.log(x&&y)
+
+console.log(x||y)
+
+const isValid=false
+console.log(isValid)
+
+console.log('priya' + 'k')
+
+const isEven=10%2===0 ? true:false
+console.log(isEven)
+
+const isEven1=10%2===0 ? 'Number is even':'Number is odd'
+console.log(isEven1)
+
+console.log(2+'3')
+console.log(true+'3')
+console.log('ytuhg'+'garer')
+console.log(2+'3NULL')
+
+
+
