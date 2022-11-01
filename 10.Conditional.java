1b6fc04ae0b3c883172a324a594ef02d957a090a
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-01 18:28:55 +0530

    Day-8 completed

---
+++ 10.Conditional.java
@@ -0,0 +1,18 @@
+for(let i=1;i<=5;i++){
+    console.log('Iteration Number in for loop'+i)
+}
+
+i=1
+while(i<=5){
+    console.log('Iteration Number in while loop'+i)
+    i++
+}
+
+
+i=1
+do{
+    console.log('Iteration Number in do while loop'+i)
+    i++
+
+}while(i<=5)
+
