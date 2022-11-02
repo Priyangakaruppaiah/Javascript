4109cb91fa07e12d3632d47011dfae74c7855cb7
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-02 19:13:53 +0530

    Day-51 completed

---
+++ 11.Switch.js
@@ -0,0 +1,15 @@
+const color='green'
+switch(color){
+    case 'red':
+        console.log('color is red')
+        break
+    case 'green':
+        console.log('color is green')
+        break 
+    case 'blue':
+        console.log('color is blue')
+        break 
+    default:
+        console.log('not a valid color')
+        break              
+}
