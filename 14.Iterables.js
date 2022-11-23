a0360f9a6cfdf018a2197f707820c1a3663b0264
PRIYANGA K <102801112+Priyangakaruppaiah@users.noreply.github.com>
2022-11-24 02:32:22 +0530

    Day-11 completed

---
+++ 14.Iterables.js
@@ -0,0 +1,8 @@
+const str="Priyanga"
+for(let i=0;i<str.length;i++){
+    console.log(str.charAt(i))
+}
+const arr=['p','r','i','y','a','n','g','a']
+for(const item of arr){
+    console.log(item)
+}
